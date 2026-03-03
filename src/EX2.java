424e3be3886ec9f09c9575d224e6ea519ea00562
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-03 20:49:12 -0300

    adição de mais coisas

--- src/EX2.java
+++ src/EX2.java
@@ -8,13 +8,25 @@ public class EX2 {
         int rm[] = new int[N];
         double nota1[] = new double[N];
         double nota2[] = new double[N];
-        int n=entradaDedados(rm, nota1,nota2);
+        int n=entradaDeDados(rm, nota1,nota2);
         for (int i=0; i<n; i++){
             System.out.println(rm[i]);
+
+        }
+        double media[] = new double[n];
+        calcularMedia(n, nota1, nota2, media);
+        for (int i = 0; i < n; i++) {
+            System.out.println("RM: " + rm[i] + "\t media: " + media[i]);
+        }
+        int aprovados[] = new int[n];
+        int nAp = listaDeAprovados(n, media, aprovados, rm);
+        System.out.println("\n **** Lista de Aprovados ****");
+        for (int i = 0; i < nAp; i++) {
+            System.out.println(aprovados[i]);
         }
 
     }
-    public static int entradaDedados( int [] rm, double[] nota1, double[] nota2){
+    public static int entradaDeDados( int [] rm, double[] nota1, double[] nota2){
         Scanner sc=new Scanner(System.in);
         int n;
         System.out.print("Informe RM (negativo para encerrar): ");
@@ -29,5 +41,21 @@ public class EX2 {
             rmLido = sc.nextInt();
         }
         return n;
+    }
+    public static void calcularMedia(int n, double[] nota1, double[] nota2, double[] media){
+
+        for (int i=0;i<n;i++) {
+            media[i] = (nota1[i] + nota2[i]) / 2;
+        }
+    }
+    public static int listaDeAprovados(int n, double[] media, int[] aprovados, int [] rm){
+        int nAp=0;
+        for (int i = 0; i < n; i++) {
+            if (media[i] >= 6) {
+                aprovados[nAp] = rm[i];
+                nAp++;
+            }
+        }
+        return nAp;
     }
 }
