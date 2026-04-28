99d2898d7ef0c6b821e5f98893032ad6bdf54aec
Rodrigo <rodrigosunzheng@gmail.com>
2026-04-28 20:11:15 -0300

    Update and rename src/fila/MainTeste.java to src/FilaString.java

---
+++ src/FilaString.java
@@ -0,0 +1,39 @@
+package fila;
+
+public class FilaString {
+   inal int N = 10;
+    String dados[] = new String[N];
+    int ini, fim, cont;
+
+    public void init(){
+        ini = fim = cont = 0;
+    }
+    public boolean isEmpty(){
+        return (cont == 0);
+    }
+    public boolean isFull(){
+        return (cont == N);
+    }
+    public void enqueue(String elem){
+        if (isFull()){
+            System.out.println("Fila cheia!");
+        }
+        else{
+            dados[fim] = elem;
+            cont++;
+            /*
+            fim++;
+            if (fim==N) {
+                fim = 0;
+            }
+             */
+            fim = (fim+1) % N;
+        }
+    }
+    public String dequeue(){
+        String elem = dados[ini];
+        cont--;
+        ini = (ini+1) % N;
+        return elem;
+    }
+}
--- src/fila/MainTeste.java
+++
@@ -1,7 +0,0 @@
-package fila;
-
-public class MainTeste {
-    public static void main(String[] args) {
-
-    }
-}
